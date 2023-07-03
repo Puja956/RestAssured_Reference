d97ae5d26c119644230e03b05b2aa7ed393efcb7
souvikmallik <souvikmallik@localhost>
2023-07-03 15:19:21 +0530

    first comment..

---
+++ src/Rest_Put_Regerence.java
@@ -0,0 +1,43 @@
+import static io.restassured.RestAssured.given;
+
+import java.time.LocalDateTime;
+
+import org.testng.Assert;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+public class Rest_Put_Regerence {
+
+	public static void main(String[] args) {
+		// Declare the base URL
+				RestAssured.baseURI = "https://reqres.in/";
+
+				// Declare the request body string variable
+				String requestBody = "{\r\n" + "    \"name\": \"morpheus\",\r\n" + "    \"job\": \"zion resident\"\r\n" + "}";
+
+				// Declare the expected results
+				JsonPath JspRequest = new JsonPath(requestBody);
+				String Req_name = JspRequest.getString("name");
+				String Req_job = JspRequest.getString("job");
+				LocalDateTime currentDate = LocalDateTime.now();
+				String expectedDate = currentDate.toString().substring(0, 11);
+
+				// declare given when & then method
+				String responseBody = given().header("Content-Type", "application/json").body(requestBody).log().all().when()
+						.put("api/users/2").then().log().all().extract().response().asString();
+
+				// Extract values from the response body
+				JsonPath JspResponse = new JsonPath(responseBody);
+				String Res_name = JspResponse.getString("name");
+				String Res_job = JspResponse.getString("job");
+				String Res_updatedAt = JspResponse.getString("updatedAt");
+				Res_updatedAt = Res_updatedAt.substring(0, 11);
+
+				// validate response body
+				Assert.assertEquals(Res_name, Req_name);
+				Assert.assertEquals(Res_job, Req_job);
+				Assert.assertEquals(Res_updatedAt, expectedDate);
+//				System.out.println(responseBody);
+			}
+		}
